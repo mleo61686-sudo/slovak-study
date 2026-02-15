8e0dc2e60f83469394a02c5ce8ac5c5ff88a25cd
Messi Leo <mleo61686@gmail.com>
2026-02-15 18:56:38 +0100

    Add image for 'mať rád'

--- app/learning/levels/a1.ts
+++ app/learning/levels/a1.ts
@@ -13,7 +13,7 @@ export const A1_ALL: Lesson[] = [
       { sk: "bývať", ua: "жити (мешкати)", ru: "жить (проживать)", img: "/words_a1/bývať.jpg" },
       { sk: "pracovať", ua: "працювати", ru: "работать", img: "/words_a1/pracovať.jpg" },
       { sk: "teší ma", ua: "приємно познайомитись", ru: "приятно познакомиться", img: "/words_a1/teší ma.jpg" },
-      { sk: "mať rád", ua: "подобатися", ru: "нравиться", img: "/words_a1/mať rad.jpg" },
+      { sk: "mať rád", ua: "подобатися", ru: "нравиться", img: "/words_a1/mať rád.jpg" },
       { sk: "ďakujem", ua: "дякую", ru: "спасибо", img: "/words_a1/ďakujem.jpg" },
     ],
   },
