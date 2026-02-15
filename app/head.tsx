81acc86a5ae16d9e80e9d9248533fa6c024b0ce4
Messi Leo <mleo61686@gmail.com>
2026-02-15 16:27:07 +0100

    Add AdSense meta verification

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,9 @@
 export default function Head() {
   return (
     <>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1760161415033749"
-        crossOrigin="anonymous"
+      <meta
+        name="google-adsense-account"
+        content="ca-pub-1760161415033749"
       />
     </>
   );
