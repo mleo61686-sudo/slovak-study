ca26bbf0de5a7155f27808572e1161ba5ef97fa1
Messi Leo <mleo61686@gmail.com>
2026-02-21 21:51:42 +0100

    adsense: add script to head

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,11 @@ export default function Head() {
         name="google-adsense-account"
         content="ca-pub-1760161415033749"
       />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1760161415033749"
+        crossOrigin="anonymous"
+      />
     </>
   );
 }
